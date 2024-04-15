bea7ac77a7fea22b48c28b71a5cd8b60cae32bff
HelloWord <d.vitova@yandex.ru>
2024-04-15 10:56:14 +0300

    work in logik

--- HelloWord/ViewController.swift
+++ HelloWord/ViewController.swift
@@ -9,11 +9,27 @@ import UIKit
 
 final class ViewController: UIViewController {
 
+    @IBOutlet var greetingLabel: UILabel!
+    @IBOutlet var greetingButton: UIButton!
+    
     override func viewDidLoad() {
-        super.viewDidLoad()
+        super.viewDidLoad() // когда экран будет загружен
+        greetingLabel.isHidden.toggle() // поменять значение greetingLabel.isHidden(скрыть) на true -
+        // по умолчанию оно false
+        greetingButton.layer.cornerRadius = 20
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func greetingButtonDidTabbed(_ sender: UIButton) {
+        greetingLabel.isHidden.toggle()
+        
+        sender.setTitle(
+            greetingLabel.isHidden ? "Show greeting" : "Hide greeting",
+            for: .normal
+        )
+        
+            
+    }
 }
+
 
